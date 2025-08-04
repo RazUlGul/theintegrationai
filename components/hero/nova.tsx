71115b9ef16715790cdad7936ae309ad1913e56f
RazUlGul <josephyang97@gmail.com>
2025-08-03 17:23:21 -0700

    Importing GitHub Repository

--- components/hero/nova.tsx
+++
@@ -1,6 +0,0 @@
-@@ .. @@
- export const NovaSections = () => {
-   const [isActive, setIsActive] = useState(false)
- }
--  const [selectedServices, setSelectedServices] = useState([])
-+  const [selectedServices, setSelectedServices] = useState<string[]>([])
