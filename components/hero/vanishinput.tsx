2e911dfbcaef84f22c013c8e3f0457e13ab56d25
RazUlGul <josephyang97@gmail.com>
2025-08-06 10:20:15 -0700

    Fix invalid diff header in vanishinput.tsx

--- components/hero/vanishinput.tsx
+++ components/hero/vanishinput.tsx
@@ -1,7 +1,6 @@
 @@ .. @@
        setConversation(prev => [
          ...prev.filter(msg => msg.message !== '...Nova is writing a response'),
-         ...parsed
        ]);
 -      
 -      // Clear services after processing the response
